90cb799d190f8eda05977f3f96b86dcc8f767e27
pc <pc@LAPTOP-HOUNK0V8>
2023-12-04 15:59:29 +0900

    Dao, Bean, 컨트롤러 기초 틀

---
+++ Trustmefollowme/src/main/java/member/model/MemberBean.java
@@ -0,0 +1,7 @@
+package member.model;
+
+import org.springframework.stereotype.Component;
+
+public class MemberBean {
+
+}
--- Trustmefollowme/src/main/java/member/model/MemberDao.java
+++ Trustmefollowme/src/main/java/member/model/MemberDao.java
@@ -1,15 +1,16 @@
-package myhotel.model;
+package member.model;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("myHotelDao")
-public class MyHotelDao {
-	
-	private final String namespace = "myhotel.Myhotel";
+@Component("memberDao")
+public class MemberDao {
+
+	private final String namespace = "member.Member";
 	
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
+	
 	
 }
